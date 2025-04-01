6f1e0e75c12b42e5bcd9da8fd0087dc1584108d3
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-04-01 11:38:19 -0700

    implemented 3 test cases

--- tests/test_student.py
+++ tests/test_student.py
@@ -1,3 +1,54 @@
 from school_schedule.student import Student
 
-# Write your tests here!
+def test_attributes_of_class_of_student():
+    #Arrange
+    name = "Alice"
+    grade = "junior"
+    classes = ["Math", "Science"]
+    
+    #Act
+    student = Student(
+        name,
+        grade,
+        classes
+    )
+    # assert
+    assert student.name == name
+    assert student.grade == grade
+    assert student.classes == classes
+    
+def test_add_class_if_class_name_is_empty():
+    #Arrange
+    name = "Alice"
+    grade = "junior"
+    classes = ["Math", "Science"]
+    class_name = None
+    
+    student = Student(
+        name,
+        grade,
+        classes
+    )
+    #Act
+    
+    new_classes = student.add_class(class_name)
+    
+    #Assert
+    assert new_classes == classes
+    
+def test_get_num_classes_length():
+    #Arrange
+    name = "Alice"
+    grade = "junior"
+    classes = ["Math", "Science"]
+    
+    student = Student(
+        name,
+        grade,
+        classes
+    ) 
+    #Act
+    length_of_classes = student.get_num_classes()
+    
+    #Assert
+    assert length_of_classes == 2
